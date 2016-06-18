8a3c8c57d7e1b079d701e8fc42c75f9b1b7f9e60
leonardoalbuquerque <leonardo.lopes.albuquerque@gmail.com>
2016-06-18 19:52:38 -0300

    adicionado teclado virtual

--- Contact.php
+++ Contact.php
@@ -3,6 +3,16 @@
 ?>
 
 <link rel="stylesheet" type="text/css" href="css/contact.css">
+<link rel="stylesheet" type="text/css" href="css/jquery-ui.min.css">
+<link rel="stylesheet" type="text/css" href="css/keyboard.css">
+
+
+<script type="text/javascript" src="js/vendors/jquery.keyboard.js"></script>
+<script type="text/javascript" src="js/vendors/jquery.mousewheel.js"></script>
+<script type="text/javascript" src="js/vendors/jquery.keyboard.extension-typing.js"></script>
+<script type="text/javascript" src="js/vendors/jquery-ui.min.js"></script>
+
+<script type="text/javascript" src="js/keyboard.js"></script>
 
 <body>
     <div class="margin">
@@ -14,11 +24,19 @@
                 chevasctba@yahoo.com.br
             </p>
 
-            <input name="name" placeholder="Nome *" type="text">
+            <div>
+                <input class="keyboard" name="name" placeholder="Nome *" type="text">
+                <img class="icon" src="https://mottie.github.io/Keyboard/docs/css/images/keyboard.png" />
+            </div>
+            <div>
+                <input class="keyboard" name="email" placeholder="Email *">
+                <img class="icon" src="https://mottie.github.io/Keyboard/docs/css/images/keyboard.png" />
+            </div>
             <br>
-            <input name="email" placeholder="Email *">
-            <br>
-            <textarea cols="50" rows="5" placeholder="Escreva aqui sua mensagem *"></textarea>
+            <div>
+                <textarea class="keyboard" cols="50" rows="5" placeholder="Escreva aqui sua mensagem *"></textarea>
+                <img class="icon" src="https://mottie.github.io/Keyboard/docs/css/images/keyboard.png" />
+            </div>
             <br>
             <input type="submit">
         </form>
